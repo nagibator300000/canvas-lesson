d47702b3f89d20cbb596d4e644cd4268764b65e2
nagibator300000 <raul.tatulyan@gmail.com>
2024-01-28 19:04:15 +0300

    50% complete AxisY

--- tasks/01-line-chart/chart/axis-x.js
+++ tasks/01-line-chart/chart/axis-x.js
@@ -1,13 +1,35 @@
 import Axis from './axis';
 
 class AxisX extends Axis {
-  constructor(canvas, ...args) {
+  constructor(...args) {
     super(...args);
+    this.TODO = null;
   }
 
   render() {
     this.ctx.beginPath();
-    this.ctx.lineTo(500);
+    this.ctx.moveTo(0, 0);
+    this.ctx.lineTo(500, 0);
+    this.ctx.stroke();
+    this.ctx.closePath();
+    this.createLabels();
+  }
+
+  createLabels() {
+    const leng = this.labels.length;
+    const ofs = 500 / leng;
+    for (let i = 1; i <= leng; i += 1) {
+      this.ctx.beginPath();
+      this.ctx.moveTo(i * ofs, -10);
+      this.ctx.lineTo(i * ofs, 10);
+      this.ctx.stroke();
+      this.ctx.closePath();
+      this.ctx.fillText(
+        `${this.labels[i - 1]}`,
+        i * ofs,
+        10 + this.labelOffset,
+      );
+    }
   }
 }
 
--- tasks/01-line-chart/chart/axis-y.js
+++ tasks/01-line-chart/chart/axis-y.js
@@ -7,7 +7,29 @@ class AxisY extends Axis {
   }
 
   render() {
-    this.TODO = undefined;
+    this.ctx.beginPath();
+    this.ctx.moveTo(0, 0);
+    this.ctx.lineTo(0, 500);
+    this.ctx.stroke();
+    this.ctx.closePath();
+    this.createLabels();
+  }
+
+  createLabels() {
+    const leng = this.labels.length;
+    const ofs = 500 / leng;
+    for (let i = 1; i <= leng; i += 1) {
+      this.ctx.beginPath();
+      this.ctx.moveTo(-10, i * ofs);
+      this.ctx.lineTo(10, i * ofs);
+      this.ctx.stroke();
+      this.ctx.closePath();
+      this.ctx.fillText(
+        `${this.labels[i - 1]}`,
+        10 + this.labelOffset,
+        i * ofs,
+      );
+    }
   }
 }
 
