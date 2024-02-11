49510975cc6b22edc731929dd929bb39bf3e607c
nagibator300000 <raul.tatulyan@gmail.com>
2024-02-11 19:08:07 +0300

    add Observer

--- tasks/02-snowfall/snow-lib/snowflake.js
+++ tasks/02-snowfall/snow-lib/snowflake.js
@@ -13,14 +13,32 @@ class Snowflake {
     this.radius = radius;
   }
 
-  render() {}
+  render() {
+    this.gradient = this.ctx.createRadialGradient(
+      this.x,
+      this.y,
+      0,
+      this.x,
+      this.y,
+      this.radius,
+    );
+    this.gradient.addColorStop(1, 'rgba(255, 255, 255, 0%)');
+    this.gradient.addColorStop(0, 'rgba(255, 255, 255, 100%)');
+    this.ctx.fillStyle = this.gradient;
+    this.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    this.ctx.fill();
+  }
 
   /**
    *
    * @param {number} deltaX
    * @param {number} deltaY
    */
-  moveBy(deltaX, deltaY) {}
+  moveBy(deltaX, deltaY) {
+    this.x += deltaX;
+    this.y += deltaY;
+    this.render();
+  }
 }
 
 export default Snowflake;
